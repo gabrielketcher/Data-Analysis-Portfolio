a1f622444ad199eef839d552c6c03e20e6a8921f
gabrielketcher <gabeketcher@gmail.com>
2025-01-10 14:08:30 -0600

    Update code.R

--- R Car Crash Clustering Analysis/code.R
+++ R Car Crash Clustering Analysis/code.R
@@ -26,3 +26,46 @@ summary(subset(myData2, aClusters == 1))
 summary(subset(myData2, aClusters == 2))
 summary(subset(myData2, aClusters == 3))
 summary(subset(myData2, aClusters == 4))
+
+
+#Gower’s coefficient R code:
+SuppressWarnings(RNGversion("3.5.3"))
+library(cluster)
+myData1 <- myData[myData$City == 'SAN DIEGO' & myData$Month %in% c(1,2),]
+myData2 <- myData1[, -c(1, 2, 3, 6, 8, 9)]
+myData2$Weekend<- ifelse(myData2$Weekday %in% c(1:5), 0, 1)
+myData2<-myData2[, -1]
+#Uses Gower’s coefficient
+d<- daisy(myData2, metric = "gower")
+aResult <- agnes(d, diss = TRUE, method = "ward")
+aResult
+plot(aResult)
+aClusters <- cutree(aResult, k = 4)
+myData2<- data.frame(myData2, aClusters)
+myData2$aClusters<-as.factor(myData2$aClusters)
+View(myData2)
+summary(subset(myData2, aClusters == 1))
+summary(subset(myData2, aClusters == 2))
+summary(subset(myData2, aClusters == 3))
+summary(subset(myData2, aClusters == 4))
+
+#3 clusters code:
+suppressWarnings(RNGversion("3.5.3"))
+library(cluster)
+myData1 <- myData[myData$City == 'SAN DIEGO' & myData$Month %in% c(1,2),]
+myData2 <- myData1[, -c(1, 2, 3, 6, 8, 9)]
+myData2$Weekend<- ifelse(myData2$Weekday %in% c(1:5), 0, 1)
+myData2<-myData2[, -1]
+d<- dist(myData2, method = "binary")
+aResult <- agnes(d, diss = TRUE, method = "ward")
+aResult
+plot(aResult)
+#Uses 3 clusters
+aClusters <- cutree(aResult, k = 3)
+myData2<- data.frame(myData2, aClusters)
+myData2$aClusters<-as.factor(myData2$aClusters)
+View(myData2)
+#Shows summaries of the 3 clusters
+summary(subset(myData2, aClusters == 1))
+summary(subset(myData2, aClusters == 2))
+summary(subset(myData2, aClusters == 3))
